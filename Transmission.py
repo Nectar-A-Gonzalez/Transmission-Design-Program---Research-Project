00949e64ba3772c9e2c0084ba3306592f3af928a
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2026-02-03 10:17:12 -0400

    Add possible direction to Transmission.py

--- Transmission.py
+++ Transmission.py
@@ -1,1 +1,3 @@
 import Inputs.py, CatalogData.py, ForceAnalysis.py, Shaft.py, Bearings.py
+# Believe this class would inherit all the others or at least use them,
+# Maybe this is where the actual code ends up running
