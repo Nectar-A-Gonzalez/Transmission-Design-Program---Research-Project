ac223a0ead8b5c10e8d06cb4a58fa03dd8f67f9a
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2026-02-03 11:03:30 -0400

    Brainstorming Shaft class inheritance structure

--- Shaft.py
+++ Shaft.py
@@ -10,3 +10,7 @@ class Shaft():
 # Subclasses (children?) or methods?
 #Clases makes the most sense since easier to set atributes to values to access
 # but if we are going to use struct, does this even matter?
+
+# ***
+# I think it makes the most sense to make the Shaft class a child of Force analysis, stress analysis, etc,
+# ***
