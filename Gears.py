561b30ee1c679086377f0f0eef1d2cf7b3950e58
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2026-02-08 19:21:18 -0400

    Added back seperate files for classes since seems possible for Inner class notation even when doing so

--- Gears.py
+++ Gears.py
@@ -1,3 +1,16 @@
+import #abstract class library as ABC
+
+# Gear from user data? for now. How to make scalable for future
+# Later it would be a decision from the ai, how to make scalable
+
+class Gears(self,#user_input.gear_type): #This could also be blank and be an output decision somewhere
+    pass
+
 # abstract class
 # Helical subclass
 # Spur gear subclass
+
+class Spur_Gears(Gears):
+    pass #redefine due to 
+
+# subclasses and Inner classes are NOT the same thing
