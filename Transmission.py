b13b346aeab957f5e72baffc170d26e487169fae
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2026-02-08 18:14:12 -0400

    Initial Idea structure for Transmission.py

--- Transmission.py
+++ Transmission.py
@@ -1,3 +1,15 @@
 import Inputs.py, CatalogData.py, ForceAnalysis.py, Shaft.py, Bearings.py
 # Believe this class would inherit all the others or at least use them,
 # Maybe this is where the actual code ends up running
+
+class Transmission:
+    def __init__(self):
+        self.dfd
+        self.dfd
+        self.dfdf
+
+        class 
+
+# Multi Inner Class structure makes sense here, but has to be acessed
+# by atributes, it seems
+# https://www.w3schools.com/python/python_class_inner.asp
