d768ddb8332c31fd9c4cfecd0ba376daabd2a3e1
Néctar A. González Negrón <nectargonzalez@outlook.com>
2026-02-19 16:04:21 -0400

    Organizing Input Class structure

--- Inputs.py
+++ Inputs.py
@@ -1,5 +1,8 @@
+# In future, would take units
+# (It would probably just convert the input values to base calculations units and convert the result before giving it)
 # would take unit type, torque in, torque out
-# In fut
-class Inputs(self,in_torque,out_torque): #MORE TO COME
-    pass
+class Inputs:
+    def __init__(self, in_torque, out_torque): #TODO: More to come
+        self.in_torque = in_torque
+        self.out_torque = out_torque
 
