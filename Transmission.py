2a99624a021166f9e461239e5e6ded9f06781838
Néctar A. González Negrón <nectargonzalez@outlook.com>
2026-02-19 16:05:51 -0400

    Organizing Transmission class structure, and class attributes for it of everything else

--- Transmission.py
+++ Transmission.py
@@ -1,16 +1,25 @@
-from Shaft.py import Shaft
-from Bearings.py import Bearings
-from Gears.py import Gears
+import Inputs
+import Shaft
+import Bearings
+import Gears
 
 #no need to import user_input nor catalog data IF this function is going to
 #actually be used inside a main.py where the process is actually run
 
-class Transmission(user_input,catalog_data):
+class Transmission:
     Shaft = Shaft # Innername = ImportedClass
     Bearings = Bearings
     Gears = Gears
+    # These are now 
 
-    def __init__(self):
+    def __init__(self,user_input:Inputs, catalog_data):
+        # P: Maybe assign all the necessary catalog data values
+        # to their rightfull class, here
+        # The functions are run in the main, usign like Transmission.Shaft(value)
+        # Verify
+        self.user_input = user_input
+        self.catalog_data = catalog_data
         self.dfd
-        self.dfd
-        self.dfdf 
+        #self.gears = Gears(###) #Example of structure
+        #self.shaft = Shaft(###)
+        #self.bearings = Bearings(###)
