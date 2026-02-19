de65a3e723361a920cd0ddd698d0eb418932c26c
Néctar A. González Negrón <nectargonzalez@outlook.com>
2026-02-19 16:04:06 -0400

    Organized possible gear inputs, attributes to store, and place to write those inputs and organze them

--- Gears.py
+++ Gears.py
@@ -3,14 +3,30 @@ import #abstract class library as ABC
 # Gear from user data? for now. How to make scalable for future
 # Later it would be a decision from the ai, how to make scalable
 
-class Gears(self,#user_input.gear_type): #This could also be blank and be an output decision somewhere
-    pass
+# What type of information is necessary of the gears?
+# - Gear type
+# - Gear amount
+# -
+# -
+# (Also, what needs to be found out)
+# - Pitch or whatever
 
-# abstract class
+class Gears: 
+    def __init__ (self, user_input_gear_type):
+        self.gear_type = user_input_gear_type
+        #self.gear_amount =
+        #
+
+    # Make all method outputs store to the class attributes, makes for easy programming and structure.
+        # Runs when object is created from class
+        pass
+
+# abstract methods i believe
+# abstract attribute of gear_type also rewritten
 # Helical subclass
 # Spur gear subclass
 
 class Spur_Gears(Gears):
-    pass #redefine due to 
+    def __init__(self, user_input_gear_type):
 
 # subclasses and Inner classes are NOT the same thing
